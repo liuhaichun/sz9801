644c155f29c6a264bcf4e5b7acaea336f5dddb29
liuhaichun <330639152@qq.com>
2020-08-18 20:43:00 +0800

    加了三行

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -7,6 +7,10 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
         System.out.println("我是老大!");
+        System.out.println("我是老大!");
+        System.out.println("我是老大!");
+        System.out.println("我是老大!");
+        System.out.println("我是老大!");
 
     }
 }
